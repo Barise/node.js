00b2e5a048921fa31e709dc4d076f866772fd84c
Barise <qkfltp01@naver.com>
2019-03-18 23:56:06 +0900

    3/18 home if-else

--- main.js
+++ main.js
@@ -2,43 +2,72 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url,true).query;
-    var title = queryData.id;
-   
-    if(_url == '/'){
-      title='Welcome';
-    }
+var app = http.createServer(function (request, response) {
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url,true).pathname;
+  var title = queryData.id;
 
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
+  
+if(pathname==='/'){
+  if(title===undefined){
+    title ='Welcome';
 
-    response.writeHead(200);
-    
-    fs.readFile(`data/${title}`,'utf8',function(err,description){
-      var template =`
-    <!doctype html>
-<html>
-<head>
-  <title>WEB1 - ${title}</title>
-  <meta charset="utf-8">
-</head>
-<body>
-  <h1><a href="/">WEB</a></h1>
-  <ul>
-    <li><a href="/?id=HTML">HTML</a></li>
-    <li><a href="/?id=CSS">CSS</a></li>
-    <li><a href="/?id=JavaScript">JavaScript</a></li>
-  </ul>
-  <h2>${title}</h2>
-  <p>${description}</p>
-</body>
-</html>
-`;
-    response.end(template);
-    })
+    var description = 'Hello, Node.js';
+      var template = `
+      
+      <!doctype html>
+      <html>
+      <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+      </head>
+      <body>
+      <h1><a href="/">WEB</a></h1>
+      <ul>
+      <li><a href="/?id=HTML">HTML</a></li>
+      <li><a href="/?id=CSS">CSS</a></li>
+      <li><a href="/?id=JavaScript">JavaScript</a></li>
+      </ul>
+      <h2>${title}</h2>
+      <p>${description}</p>
+      </body>
+      </html>
+      `;
+      //http상태코드 2xx은 성공
+      response.writeHead(200);
+      response.end(template);
+       }
+         else{
+        fs.readFile(`data/${title}`, 'utf8', function (err, description) {
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+          </head>
+          <body>
+          <h1><a href="/">WEB</a></h1>
+          <ul>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ul>
+          <h2>${title}</h2>
+          <p>${description}</p>
+          </body>
+          </html>
+          `;
+          //http상태코드 2xx은 성공
+          response.writeHead(200);
+          response.end(template);
+            });
+       }
+    } else {
+      response.writeHead(404);
+      response.end("Not found");
+    } 
 
 });
 app.listen(3000);
