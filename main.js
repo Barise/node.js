ab241b31e9a75b546457f05f565168bcdd276800
Barise <qkfltp01@naver.com>
2019-03-19 11:17:12 +0900

    3/19 company filelist 완

--- main.js
+++ main.js
@@ -7,40 +7,28 @@ var app = http.createServer(function (request, response) {
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url,true).pathname;
   var title = queryData.id;
-
+  var testFolder = './data/';
   
-if(pathname==='/'){
-  if(title===undefined){
-    title ='Welcome';
+    if(pathname==='/'){
+      //파일 목록 읽음
+      fs.readdir(testFolder,function(error,filelist){
+        var list='<ul>';
+        var i = 0;
 
-    var description = 'Hello, Node.js';
-      var template = `
-      
-      <!doctype html>
-      <html>
-      <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-      </head>
-      <body>
-      <h1><a href="/">WEB</a></h1>
-      <ul>
-      <li><a href="/?id=HTML">HTML</a></li>
-      <li><a href="/?id=CSS">CSS</a></li>
-      <li><a href="/?id=JavaScript">JavaScript</a></li>
-      </ul>
-      <h2>${title}</h2>
-      <p>${description}</p>
-      </body>
-      </html>
-      `;
-      //http상태코드 2xx은 성공
-      response.writeHead(200);
-      response.end(template);
-       }
-         else{
-        fs.readFile(`data/${title}`, 'utf8', function (err, description) {
+        while(i<filelist.length-1){
+          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+          i++;
+        }
+
+        list = list+'</ul>';
+        //index page
+      if(title===undefined){
+        title ='Welcome';
+        var description = 'Hello, Node.js';
+
+        
           var template = `
+          
           <!doctype html>
           <html>
           <head>
@@ -49,11 +37,7 @@ if(pathname==='/'){
           </head>
           <body>
           <h1><a href="/">WEB</a></h1>
-          <ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
+          ${list}
           <h2>${title}</h2>
           <p>${description}</p>
           </body>
@@ -62,12 +46,37 @@ if(pathname==='/'){
           //http상태코드 2xx은 성공
           response.writeHead(200);
           response.end(template);
-            });
-       }
-    } else {
-      response.writeHead(404);
-      response.end("Not found");
-    } 
+          }
 
-});
+          //url
+          else{
+              fs.readFile(`${testFolder}${title}`,'utf8',function(error,description){
+              var template = `
+                <!doctype html>
+                <html>
+                <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+                </head>
+                <body>
+                <h1><a href="/">WEB</a></h1>
+                ${list}
+                <h2>${title}</h2>
+                <p>${description}</p>
+                </body>
+                </html>
+              `;
+              //http상태코드 2xx은 성공
+              response.writeHead(200);
+              response.end(template);
+                })
+              }
+            })
+        } 
+        //예외처리
+        else {
+          response.writeHead(404);
+          response.end("Not found");
+        } 
+    });
 app.listen(3000);
